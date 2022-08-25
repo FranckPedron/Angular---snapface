c733ea04013766e2dfd73e0a449036fa859c43c7
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-08-25 22:59:14 +0200

    Build - add conditional display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,32 @@ import {FaceSnap} from "./models/face-snap.model";
 })
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Démo',
-      'Petite démo',
-      new Date(),
-      0,
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    );
-
+    this.mySnap = {
+      title: 'Démo',
+      description: 'Petite démo',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      location: 'la campagne'
+    };
+    this.myOtherSnap = {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'la montagne'
+    };
+    this.myLastSnap = {
+      title: 'Un bon repas',
+      description: 'Mmmh que c\'est bon !',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    };
   }
 }
