472a93f27298ac90f1d90a46ed9f59d22c04a084
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-08-25 16:23:50 +0200

    Build - first binding

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit() {
+    this.title = 'Demo';
+    this.description = 'Petite démo';
+    this.createDate = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+}
