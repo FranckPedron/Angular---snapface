dc468a88a3a8b7c6db86c0afdd8f3da8b0178e02
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-08-29 16:18:57 +0200

    Build - add navigation between snaps+

---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {FaceSnap} from "../models/face-snap.model";
+import {FaceSnapsService} from "../services/face-snaps.service";
+
+@Component({
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
+})
+export class SingleFaceSnapComponent implements OnInit {
+  faceSnap!: FaceSnap;
+  snapped!: boolean;
+  buttonText!: string;
+
+  constructor(private faceSnapsService: FaceSnapsService,
+              private route: ActivatedRoute) {}
+
+    ngOnInit() {
+    const faceSnapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
+    this.snapped = false;
+    this.buttonText = 'Snap !';
+  }
+
+  onClickSnap() {
+    if (this.snapped === false) {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.buttonText = 'Oops, un Snap!';
+      this.snapped = true;
+    } else {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+      this.buttonText = 'Snap!';
+      this.snapped = false;
+    }
+  }
+}
