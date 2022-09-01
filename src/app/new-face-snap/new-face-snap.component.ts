aa45afcef07c09c16c6e9dd08d0956c283d79486
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-09-01 11:51:19 +0200

    Build - add reactive forms

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {map, Observable} from "rxjs";
+import {FaceSnap} from "../models/face-snap.model";
 
 @Component({
   selector: 'app-new-face-snap',
@@ -7,7 +9,9 @@ import {FormBuilder, FormGroup} from "@angular/forms";
   styleUrls: ['./new-face-snap.component.scss']
 })
 export class NewFaceSnapComponent implements OnInit {
+
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -19,6 +23,15 @@ export class NewFaceSnapComponent implements OnInit {
       imageUrl: [null],
       location: [null]
     });
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdDate: new Date(),
+        id: 0,
+        snaps: 0
+      }))
+    );
   }
 
   onSubmitForm(): void {
