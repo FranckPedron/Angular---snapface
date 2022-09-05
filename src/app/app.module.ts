395fd2865e564fe2ea449644be54be523fb2ddb1
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-09-05 10:44:19 +0200

    Build - add lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import {NgModule, LOCALE_ID} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {CoreModule} from "./core/core.module";
-import {FaceSnapsModule} from "./face-snaps/face-snaps.module";
 import {LandingPageModule} from "./landing-page/landing-page.module";
 
 @NgModule({
@@ -14,9 +13,9 @@ import {LandingPageModule} from "./landing-page/landing-page.module";
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    FaceSnapsModule,
     LandingPageModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/app/face-snaps/face-snaps-routing.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {FaceSnapListComponent} from "./components/face-snap-list/face-snap-list.component";
+import {SingleFaceSnapComponent} from "./components/single-face-snap/single-face-snap.component";
+import {NewFaceSnapComponent} from "./components/new-face-snap/new-face-snap.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'create', component: NewFaceSnapComponent},
+  {path: '', component: FaceSnapListComponent},
+  {path: ':id', component: SingleFaceSnapComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class FaceSnapsRoutingModule {
+
+}
